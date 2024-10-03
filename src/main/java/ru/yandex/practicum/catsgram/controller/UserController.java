360e778ae89554dbfe4d1586cd052fb4fcb22824
Igor217 <rkjdth.ctv@yandex.ru>
2024-10-03 07:21:47 +0300

    котограм 2

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -0,0 +1,71 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
+import ru.yandex.practicum.catsgram.model.User;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class UserController {
+
+    private final Map<Long, User> users = new HashMap<>();
+
+
+    @GetMapping("/users")
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping("/users")
+    public User create(@RequestBody User user) {
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
+            throw new ConditionsNotMetException("Email не может быть пустым");
+        }
+        for (User u : users.values()) {
+            if (u.getEmail().equals(user.getEmail())) {
+                throw new DuplicatedDataException("Email уже используется");
+            }
+        }
+        user.setId(getNextId());
+        user.setRegistrationDate(Instant.now());
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping("/users")
+    public User update(@RequestBody User newUser) {
+        if (newUser.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+        User existingUser = users.get(newUser.getId());
+        if (newUser.getEmail() != null && !newUser.getEmail().isBlank() && !existingUser.getEmail().equals(newUser.getEmail())) {
+            for (User u : users.values()) {
+                if (u.getEmail().equals(newUser.getEmail())) {
+                    throw new DuplicatedDataException("Этот имейл уже используется");
+                }
+            }
+            existingUser.setEmail(newUser.getEmail());
+        }
+        if (newUser.getUsername() != null && !newUser.getUsername().isBlank()) {
+            existingUser.setUsername(newUser.getUsername());
+        }
+        if (newUser.getPassword() != null && !newUser.getPassword().isBlank()) {
+            existingUser.setPassword(newUser.getPassword());
+        }
+        return existingUser;
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
--- src/main/java/ru/yandex/practicum/catsgram/model/Post.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/Post.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.catsgram.controller.model;
+package ru.yandex.practicum.catsgram.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
